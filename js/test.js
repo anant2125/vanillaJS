560d7b7b1264e5ce36e7d389c6ff78f12b0ed63a
Anant Kumar <anantraj2125@gmail.com>
2018-10-21 21:01:37 +0530

    before dinner code

---
+++ js/test.js
@@ -0,0 +1,14 @@
+var name='Anant'
+var count=0;
+myFunction=function()
+{
+    window.alert("Welcome "+name)
+};
+
+
+testApp=function()
+{
+    if(count===0)
+        window.alert("yay!!! You Have Nothing to DO...!!!")
+
+};
